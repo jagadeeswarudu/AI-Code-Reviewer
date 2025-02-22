1b9ff7983780c5a59ee6c2c906a6636b7a1231e0
Jagadeeswarudu <jagat0963@gmail.com>
2025-02-22 11:37:36 +0530

    added requirements and unwanted code changes

--- app.py
+++ app.py
@@ -26,31 +26,3 @@ if st.button("Generate"):
         st.write(res)
     else:
         st.warning("Please enter code before Generating!")
-# from openai import OpenAI
-
-# OpenAI API Key (replace with your key or use environment variable for security)
-
-# key = "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"
-# c=OpenAI(api_key=key)
-# c.models.list()
-# import openai
-# import os
-
-# # Access the API key securely from the environment
-# openai.api_key = "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"
-
-# # List models
-# models = openai.Model.list()
-# for model in models['data']:
-#     print(model['id'])
-
-# import google.generativeai as genai
-# key="AIzaSyAHi-rg6OcKjfcXm-QNYrOP7HXVJE-xMYE"
-# genai.configure(api_key=key)
-# model=genai.GenerativeModel(model_name="gemini-1.5-flash")
-
-# prompt="""You are an expert Python developer. Please review the following code for bugs, errors, or inefficiencies:
-#     code is: print("hello"}
-#     Provide bug report and fixed code only."""
-# response=model.generate_content(prompt)
-# print(response.text)
